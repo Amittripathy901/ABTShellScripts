08874ebaf133031e99028d13aaf4c67353e3a68b
Amittripathy901 <amittripathy.work@gmail.com>
2025-01-28 14:53:36 +0000

    Add variable.sh

--- variable.sh
+++ variable.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
+#Taking input from another file
 
-#script to show how to use variables
+read HOSTNAME < /etc/hostname
 
+#printing the value
 
-a=10
-name="Amit"
-age=21
-
-echo "My name is $name and my age is $age"
-
-name="Paul"
-
-echo "My name is $name"
-
-#variable to store output of a command
+echo Hostname $HOSTNAME
 
 
-HOSTNAME=$(hostname)
-echo "Name of this machine is $HOSTNAME"
